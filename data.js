e5488e06648fb9f3b72bc050469cf83577d89855
Faton Jonuzi <143906241+tonny-27@users.noreply.github.com>
2024-02-01 14:32:11 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,59 @@
+export const tweetsData = [   
+    {
+        handle: `@ChuckNorris ✅`,
+        profilePic: `images/chucknorris.jpeg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Imagine if I could get paid for sleeping but dreams 
+        were replaced with ads!!! 😎
+        `,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Elon ✅`,
+        profilePic: `images/musk.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `If I die under mysterious circumstance, it's bee nice knowin ya 🚀`,
+        replies: [
+                  {
+                handle: `@TomCruise ✅`,
+                profilePic: `images/tcruise.png`,
+                tweetText: `If that happens can I have X? 😎🛩`,
+            },
+                  {
+                handle: `@Elon ✅`,
+                profilePic: `images/musk.png`,
+                tweetText: `Ok`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@NoobCoder12`,
+        profilePic: `images/flower.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
+            },
+            {
+                handle: `@YummyCoder64`,
+                profilePic: `images/love.png`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
